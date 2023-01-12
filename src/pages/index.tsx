01400fb2467b92dcba8f88f61d50d0725d841744
Darya Kozlova <DSKozlova5@yandex.ru>
2023-01-12 18:25:30 +0300

    change backgound with location indicator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,22 +72,28 @@ const Home = () => {
             >
                <div className="mx-auto max-w-full sm:pb-36 sm: lg:pt-8">
 
-                  <div>
-                     <div className="bg-white leading-10 mx-5 lg:mx-32 border-darkblue-700 border-2">
+                  <div className="mb-6">
+                     <div className="leading-10">
                         <div className="flex items-center justify-center">
-                           <p className="text-center text-darkblue-300 font-bold">Готовность к приключениям загружается...                     </p>
+                           <p className="text-center text-darkblue-300 font-bold bg-white/90">Готовность к приключениям загружается...                     </p>
                            <svg aria-hidden="true" className="w-6 h-6 ml-1 mb-1 text-darkblue-600 animate-spin fill-darkblue-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                               <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                            </svg>
 
                         </div>
-                        <h1 className="text-3xl text-center font-extrabold leading-10">
+                        <div>
+                           <div className="flex items-center justify-center rounded-lg">
+                        <h1 className="text-3xl text-center font-extrabold leading-10 bg-white/90 mx-2">
                            Что за квест вам предстоит пройти?
                         </h1>
-                        <h2 className="text-xl text-center font-bold leading-9">
+                        </div>
+                        <div className="flex items-center justify-center rounded-lg">
+                        <h2 className="text-xl text-center font-bold leading-9 bg-white/90">
                            Давайте узнаем!
                         </h2>
+                        </div>
+                        </div>
                      </div>
                      <div className="grid gap-12 pt-10 pb-7 text-center grid-flow-row auto-rows-max lg:grid-flow-col lg:grid-rows-2">
                         <div className="bg-darkblue-400 text-white">
@@ -132,11 +138,11 @@ const Home = () => {
                         <button className="bg-gold-300 font-bold py-2 px-4 border-b-4 border-b-gold-500 rounded text-darkblue-400 ">Уже готов купить за 800 рублей</button>
                      </div>
                      <div className="flex justify-center">
-                        <p className="md:text-xs lg:text-sm pt-1 bg-white">цена для компании от 1 до 8 человек</p>
+                        <p className="md:text-xs lg:text-sm pt-1 bg-white/80">цена для компании от 1 до 8 человек</p>
                      </div>
                   </div>
 
-                  <div>
+                  <div className="mb-12">
                      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8">
                         <h2 className="sr-only">Products</h2>
 
@@ -160,18 +166,33 @@ const Home = () => {
                            ))}
                         </div>
                      </div>
+                     <div className="flex justify-center">
+                     <Link
+                        href="/routesAndPrices"
+                        className="bg-darkblue-400 align-bottom rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-gold-300 md:mx-1 lg:mx-2"
+                     >
+                        Выбрать квест
+                        <span className="inline-block align-bottom">
+                           <Image alt="Панда" src={panda} className="pl-1" width={35} />
+                        </span>
+                     </Link>
+                     </div>
                   </div>
 
                   <div>
-                     <section className="mb-20 text-gray-700">
-                        <div className="text-center bg-white md:max-w-xl lg:max-w-3xl mx-auto">
-                           <h3 className="text-3xl font-bold mb-6 text-gray-800">Отзывы</h3>
-                           <p className="mb-6 pb-2 md:mb-12 md:pb-0">
-                              Краткий обзор трех отзывов от наших клиентов. Если хотите почитать все отзывы - <Link href="/reviews" className="text-darkblue-300">переходите по ссылке</Link>.
+                     <section className="mb-20">
+                        <div className=" bg-white/90 md:max-w-xl lg:max-w-3xl mx-auto">
+                           <div className="flex justify-center">
+                           <h3 className="flex justify-center text-3xl font-bold mb-6 ">Отзывы</h3>
+                           </div>
+                           <div>
+                           <p className="flex justify-center mb-6 pb-2 md:mb-12 md:pb-0">
+                              Краткий обзор трех отзывов от наших клиентов.
                            </p>
+                           </div>
                         </div>
 
-                        <div className="grid bg-white md:grid-cols-3 gap-6 lg:gap-12 text-center">
+                        <div className="grid bg-white/90 md:grid-cols-3 gap-6 lg:gap-12 text-center">
                            <div className="mb-12 md:mb-0">
                               <div className="flex justify-center mb-6">
                                  <Image alt="Клиент №1"
@@ -267,20 +288,12 @@ const Home = () => {
 
 
                   <div className="mt-8 flex gap-x-4 sm:justify-center">
-                     <Link
-                        href="/routesAndPrices"
-                        className="bg-darkblue-400 align-bottom rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-gold-300 md:mx-1 lg:mx-2"
-                     >
-                        Выбрать квест
-                        <span className="inline-block align-bottom">
-                           <Image alt="Панда" src={panda} className="pl-1" width={35} />
-                        </span>
-                     </Link>
+
                      <Link
                         href="/reviews"
                         className="bg-gold-300 inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-brown-400 ring-1 ring-darkblue-300 md:mx-1 lg:mx-2"
                      >
-                        Почитать отзывы
+                        Почитать другие отзывы
                         <span className="inline-block align-bottom">
                            <Image alt="Бурый медведь" src={darkBear} className="pl-1" width={35} />
                         </span>
